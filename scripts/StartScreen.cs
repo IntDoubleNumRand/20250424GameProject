4aee7a2a5e177af2c3a17691bac583353ac1c838
Yi <ysun1@seattleu.edu>
2025-06-09 14:38:59 -0700

    Added ending shortcut for demo

--- scripts/StartScreen.cs
+++ scripts/StartScreen.cs
@@ -25,20 +25,40 @@ public partial class StartScreen : Control
 		startButton.Position = new Vector2(100, 160);
 		startButton.Pressed += OnStartPressed;
 		AddChild(startButton);
+		
+		var demoVictoryButton = new Button { Name = "DemoVictoryButton", Text = "Demo Victory" };
+		demoVictoryButton.Position = new Vector2(100, 200);
+		demoVictoryButton.Pressed += OnDemoVictoryPressed;
+		AddChild(demoVictoryButton);
+		
+		var demoGameOverButton = new Button { Name = "DemoGameOverButton", Text = "Demo Game Over" };
+		demoGameOverButton.Position = new Vector2(250, 200);
+		demoGameOverButton.Pressed += OnDemoGameOverPressed;
+		AddChild(demoGameOverButton);
 	}
-
+	
 	private void OnStartPressed()
 	{
 		int sheepCount = int.Parse(GetNode<LineEdit>("SheepInput").Text);
 		int wolfCount = int.Parse(GetNode<LineEdit>("WolfInput").Text);
-
+		
 		var gameSettings = new Godot.Collections.Dictionary<string, int>
 		{
 			{ "sheep", sheepCount },
 			{ "wolf", wolfCount }
 		};
-
+		
 		GetTree().Root.SetMeta("GameSettings", gameSettings);
 		GetTree().ChangeSceneToFile("res://scenes/Stage.tscn");
+	}
+	
+	private void OnDemoVictoryPressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/VictoryScreen.tscn");
+	}
+	
+	private void OnDemoGameOverPressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/GameOverScreen.tscn");
 	}
 }
